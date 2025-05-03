c81ace99ba9e7b518f8e69da28914c9335577866
Andrii Kulikov <akulikov@ebsco.com>
2025-05-03 00:40:46 -0400

    Add icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'cardcopy',
+        short_name: 'Card Copy',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
